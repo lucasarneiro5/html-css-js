076201faeba090b300f2891aca1085a22ee7fcf8
Lucas Arneiro <lucasarneiro.v@gmail.com>
2023-04-23 16:51:28 -0300

    Menu Responsivo

---
+++ Menu-Resposivo/basica/exemplo.js
@@ -0,0 +1,7 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const navigation = document.querySelector('.navigation');
+
+menuToggle.addEventListener('click', () => {
+	menuToggle.classList.toggle('active');
+	navigation.classList.toggle('active');
+});
